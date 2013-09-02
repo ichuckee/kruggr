e293e8bd67727340651e34e90da65dbbece84ce8
Collins Iheagwara <collinsiheagwara@gmail.com>
2013-09-02 21:56:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kruggr::Application.config.secret_token = '1a1a1276989a29a18778958dd0376af4378cc80c5390c16a50da9e56a97222a42e382184354a2717569b44eaa66c7d883ceb5d8705495a808816f463503b2819'
